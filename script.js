30fddfa9056e2a33931a5be87d7a590e38df1e9f
adityash12 <128187345+adityash12@users.noreply.github.com>
2023-10-29 01:08:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function decimalToBinary(num) {
+	let output=""; 
+
+	while(num>=2){
+      let rem= num%2;
+		output=output+rem;
+		num = pareseInt(num/2);
+		
+	}
+	if(num==1){
+     output= 1+ output;
+	}
+	return output;
+	
   //Write you code here
   
 }
